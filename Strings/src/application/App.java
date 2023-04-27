4f96e1da0a7d2d2a0de9b328929b2e82485d35f3
Fabio Schapowal <fabiosw@me.com>
2023-04-27 06:55:36 +0100

    String

---
+++ Strings/src/application/App.java
@@ -0,0 +1,26 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		String name = "Fabio";
+		
+		String frase = "\tHello, \nhow are you?";
+		
+		double value = 123;
+		
+		
+		System.out.println(name);
+		System.out.println(frase);
+		System.out.print(frase);
+		System.out.printf("%.2f", value);
+		System.out.printf("%d", 777); //decimal
+		System.out.printf("%s %d", "string", 777);
+		
+		
+
+		
+	}
+
+}
