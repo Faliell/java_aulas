084690f0e0a556f28f2d9eac7570fcd472202629
Fabio Schapowal <fabiosw@me.com>
2023-05-02 06:02:30 +0100

    boolean

---
+++ Boolean/src/application/App.java
@@ -0,0 +1,18 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		boolean verificar = true;
+		
+		// &b boolean
+		System.out.printf("printar %b%n", verificar);
+		
+		// Não usar == para float
+		
+		System.out.printf("10 é igual 3 %b", 10 == 3);
+
+	}
+
+}
