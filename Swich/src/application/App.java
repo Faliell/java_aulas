f6de4388996eb42922f7276bfa0e7fc1b75cb039
Fabio Schapowal <fabiosw@me.com>
2023-05-03 05:52:23 +0100

    switch

---
+++ Swich/src/application/App.java
@@ -0,0 +1,26 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		int choice = 0;
+		
+		switch(choice) {
+		case 0:
+			System.out.println("a");
+			break;
+		case 1:
+			System.out.println("b");
+			break;
+		case 2:
+			System.out.println("c");
+			break;
+		default:
+			System.out.println("d");
+			break;
+		}
+		
+
+	}
+
+}
