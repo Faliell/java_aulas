8cc3126fada7b84daf528bf9d06d2f7767e612b1
Fabio Schapowal <fabiosw@me.com>
2023-05-03 06:31:42 +0100

    doWhile

---
+++ doWhile/src/application/App.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		int count = 0;
+		
+		do {
+			System.out.println(count);
+		}
+	
+		while (count++ < 10);
+		
+
+		
+	}
+
+}
