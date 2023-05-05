38479a5a8f48b47266c967f290adca02a417fae3
Fabio Schapowal <fabiosw@me.com>
2023-05-05 06:04:53 +0100

    escope

---
+++ Escope/src/application/App.java
@@ -0,0 +1,14 @@
+package application;
+
+public class App {
+	public static void main(String[] args) {
+		
+		int count = 0;
+		while(count < 10) {
+			System.out.printf("Count: %d\n", count);
+			count += 1;
+		}
+	
+
+	}
+}
