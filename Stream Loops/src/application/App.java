2ea60758a47a47ffefe40fbb1c05a8c9124dabff
Fabio Schapowal <fabiosw@me.com>
2023-05-08 07:30:04 +0100

    array

--- Stream Loops/src/application/App.java
+++ Stream Loops/src/application/App.java
@@ -1,12 +1,12 @@
 package application;
 
-import java.util.stream.IntStream;
+
 
 public class App {
 	public static void main(String[] args) {
 		
-		IntStream.range(0,5).forEach(i ->{
-			System.out.println(i);
-		});
+		int[] numbers = {1,4,5,2,7};
+		
+		System.out.println(numbers[0]);
 	}
 }
