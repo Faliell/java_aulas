27ebce9cfe0e0234e3e3584c34eaeb232f655350
Fabio Schapowal <fabiosw@me.com>
2023-05-08 07:40:29 +0100

    iterate

--- Stream Loops/src/application/App.java
+++ Stream Loops/src/application/App.java
@@ -8,5 +8,18 @@ public class App {
 		int[] numbers = {1,4,5,2,7};
 		
 		System.out.println(numbers[0]);
+		
+		String[] words = {"apple", "banana", "mango"};
+		
+		System.out.println(words[0]);
+		
+		//interate:
+		
+		for(int i=0; i<words.length; i++) {
+			System.out.println(words[i]);
+		}
+			
+		
+			
 	}
 }
