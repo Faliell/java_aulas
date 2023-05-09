5ea1339e09bac8c785fe241361e0d7c9c2e7b174
Fabio Schapowal <fabiosw@me.com>
2023-05-09 06:13:38 +0100

    list for

--- Lists Stream Loops/src/application/App.java
+++ Lists Stream Loops/src/application/App.java
@@ -1,14 +1,33 @@
 package application;
 
+import java.util.stream.IntStream;
+
 public class App {
 	public static void main(String[] args) {
-		
-		// criar lista com tamanho determinado:
-		int[] numbers = new int[3];
-		
-		for(int i = 0; i < numbers.length; i++) {
-			System.out.println(numbers[i]);
+
+		int[] numbers = { 1, 4, 5, 2, 7 };
+
+		System.out.println(numbers[0]);
+
+		String[] words = { "apple", "banana", "mango" };
+
+		System.out.println(words[0]);
+
+		// iterate:
+
+		for (int i = 0; i < words.length; i++) {
+			System.out.println(words[i]);
 		}
-			
+		// iterate stream:
+		IntStream.range(0, words.length).forEach(i -> {
+			System.out.printf("%d. %s %n", i + 1, words[i]);
+		});
+		
+		//iterate for:
+		
+		for(String word: words) {
+			System.out.println(word);
+		}
+
 	}
 }
