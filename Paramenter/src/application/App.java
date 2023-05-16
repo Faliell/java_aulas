bd32ce259cd1c668626fb9a266b2719b0ce47949
Fabio Schapowal <fabiosw@me.com>
2023-05-16 05:59:53 +0100

    parameters

--- Paramenter/src/application/App.java
+++ Paramenter/src/application/App.java
@@ -6,6 +6,15 @@ public class App {
 		Calculator result = new Calculator();
 		int num = result.square(2);
 		System.out.println(num);
+		
+		int soma = result.add(3,4);
+		System.out.println(soma);
+		
+		int sub = result.sub(6,5);
+		System.out.println(sub);
+		
+		int multi = result.multi(soma, sub);
+		System.out.println(multi);
 	}
 
 }
--- Paramenter/src/application/Calculator.java
+++ Paramenter/src/application/Calculator.java
@@ -4,5 +4,17 @@ public class Calculator {
 	int square(int num) {
 		return num * num;
 	}
+
+	int add(int i, int j) {
+		return i + j;
+	}
+
+	int sub(int i, int j) {
+		return i - j;
+	}
+	
+	int multi(int a, int b) {
+		return a * b;
+	}
 	
 }
