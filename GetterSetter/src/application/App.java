a2ffa5c6d1482382bae3bbd57914aa0726b13573
Fabio Schapowal <fabiosw@me.com>
2023-05-16 06:15:21 +0100

    getter and setter

---
+++ GetterSetter/src/application/App.java
@@ -0,0 +1,15 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		Person person1 = new Person();
+		
+		person1.setName("Maria");
+		person1.setAge(30);
+		System.out.println(person1.name);
+		System.out.println(person1.age);
+
+	}
+
+}
---
+++ GetterSetter/src/application/Person.java
@@ -0,0 +1,23 @@
+package application;
+
+public class Person {
+	int age;
+	String name;
+	
+	void setAge(int theAge) {
+		age = theAge;
+	}
+	
+	void setName(String theName) {
+		name = theName;
+	}
+	
+	String getName() {
+		return name;
+	}
+	
+	int	getAge() {
+		return age;
+	}
+
+}
