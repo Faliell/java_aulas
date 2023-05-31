46716440ddc950b8540d326c26cf5985ea003293
Fabio Schapowal <fabiosw@me.com>
2023-05-31 06:04:48 +0100

    and

---
+++ BooleanAndOr/src/application/App.java
@@ -0,0 +1,25 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		boolean isRaining = true;
+		boolean haveUmbrella = true;
+		
+		boolean takeUmbrella = false;
+		
+		if(isRaining) {
+			if(haveUmbrella) {
+				takeUmbrella = true;
+			}
+		}
+		
+		if(isRaining && haveUmbrella) {
+			takeUmbrella = true;
+		}
+		
+		
+		System.out.println(takeUmbrella);
+	}
+
+}
