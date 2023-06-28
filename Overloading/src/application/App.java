5457be92b44ab8cbb72fa907da6a3842d6366030
Fabio Schapowal <fabiosw@me.com>
2023-06-28 06:59:16 +0100

    overloading

---
+++ Overloading/src/application/App.java
@@ -0,0 +1,11 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		Person person1 = new Person();
+		person1.greet();
+		person1.greet("joe");
+	}
+
+}
---
+++ Overloading/src/application/Person.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Person {
+	public void greet() {
+		System.out.println("hi");
+	}
+	public void greet(String nameToGreet){
+		System.out.println("Hi" + nameToGreet);
+	}
+
+}
