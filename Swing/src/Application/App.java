27f9df87efab8e0ea17915f7b272354ef5871c9c
Fabio Schapowal <fabiosw@me.com>
2023-06-29 06:54:50 +0100

    swing

---
+++ Swing/src/Application/App.java
@@ -0,0 +1,16 @@
+package Application;
+
+import javax.swing.JFrame;
+
+public class App {
+
+	public static void main(String[] args) {
+		JFrame mainFrame = new JFrame();
+		
+		mainFrame.setVisible(true);
+		mainFrame.setSize(600,400);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
