7251036e8016181ee09737cce4514950d37c0176
Fabio Schapowal <fabiosw@me.com>
2023-07-03 07:20:14 +0100

    access modifier

---
+++ AccessModifier/src/application/App.java
@@ -0,0 +1,10 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		float nome = 111;
+		System.out.println(nome);
+	}
+
+}
--- Swing/src/Application/App.java
+++ Swing/src/Application/App.java
@@ -1,15 +1,20 @@
 package Application;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class App {
 
 	public static void main(String[] args) {
-		JFrame mainFrame = new JFrame();
 		
-		mainFrame.setVisible(true);
-		mainFrame.setSize(600,400);
-		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		SwingUtilities.invokeLater(() -> {
+			JFrame mainFrame = new JFrame();
+			
+			mainFrame.setVisible(true);
+			mainFrame.setSize(600,400);
+			mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		});
+		
 
 	}
 
