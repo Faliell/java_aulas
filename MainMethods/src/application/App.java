7a8b017122506555764fc87681361cb48c445923
Fabio Schapowal <fabiosw@me.com>
2023-07-04 06:33:15 +0100

    main method

---
+++ MainMethods/src/application/App.java
@@ -0,0 +1,13 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		
+		for(var arg: args) {
+			System.out.println(arg);
+		}
+	}
+
+}
