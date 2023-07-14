269c2f4d4639dcdd8224870b8ee8374671dff4e6
Fabio Schapowal <fabiosw@me.com>
2023-07-14 07:54:41 +0100

    interface exemple

---
+++ InterfaceExemple/src/application/App.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+class Task implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		
+	}
+	
+}
+
+public class App {
+
+	public static void main(String[] args) {
+		ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+		
+		executor.scheduleAtFixedRate(new Task(), 0, 1, TimeUnit.SECONDS);
+	}
+
+}
